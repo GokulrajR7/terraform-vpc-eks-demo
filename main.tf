499909764198bb1f76311ae5a8103c34c154e224
GokulrajR7 <gokulrajeswaran2005@gmail.com>
2026-02-24 19:18:38 +0000

    added complete vpc setup

--- main.tf
+++ main.tf
@@ -3,6 +3,40 @@ provider "aws"{
 region="ap-south-1"
 }
 
+
+# -------------------------------
+# Terraform Backend (S3 + DynamoDB)
+# -------------------------------
+terraform {
+  backend "s3" {
+    bucket         = "terraform-vpc-eks-statefile-bucket-gokul" # must be globally unique
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
+
+
+
+
+# DynamoDB Table for Locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Locks"
+  }
+}
+
+
 #VPC
 
 resource "aws_vpc" "main"{
@@ -10,25 +44,85 @@ cidr_block="10.0.0.0/16"
 
 enable_dns_support=true
 enable_dns_hostnames=true
+tags={
+  Name="main-vpc"}
 }
 
 #SUBNETS 
 
 #SUBNET1
 
-resource "aws_subnet" "subnet1"{
+resource "aws_subnet" "public"{
 vpc_id=aws_vpc.main.id
 cidr_block="10.0.1.0/24"
 availability_zone="ap-south-1a"
+map_public_ip_on_launch=true
+tags={ Name="public-subnet"}
+
 }
 
 #SUBNET2
 
-resource "aws_subnet" "subnet2"{
+resource "aws_subnet" "private"{
 vpc_id=aws_vpc.main.id
 cidr_block="10.0.2.0/24"
 availability_zone="ap-south-1b"
+tags={Name="private-subnet"}
+
 }
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "igw"{
+  vpc_id=aws_vpc.main.id
+  tags={Name="main-igw"}
+}
+
+
+# Nat Gateway
+
+resource "aws_eip" "nat_eip"{
+  tags={ Name="nat-eip"}
+}
+
+resource "aws_nat_gateway" "nat"{
+  allocation_id=aws_eip.nat_eip.id
+  subnet_id=aws_subnet.public.id
+  tags={ Name="main-nat"}
+}
+
+
+# Route Tables
+
+resource "aws_route_table" "public_rt"{
+  vpc_id=aws_vpc.main.id
+  route{
+    cidr_block="0.0.0.0/0"
+    gateway_id=aws_internet_gateway.igw.id
+  }
+  tags={ Name="public-rt"}
+}
+
+resource "aws_route_table_association" "public_assoc"{
+  subnet_id=aws_subnet.public.id
+  route_table_id=aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt"{
+  vpc_id=aws_vpc.main.id
+  route{
+    cidr_block="0.0.0.0/0"
+    nat_gateway_id=aws_nat_gateway.nat.id
+  }
+  tags={ Name="private-rt" }
+}
+
+
+resource "aws_route_table_association" "private_assoc"{
+  subnet_id=aws_subnet.private.id
+  route_table_id=aws_route_table.private_rt.id
+}
+
 
 #IAM ROLE FOR EKS
 
@@ -68,8 +162,8 @@ role_arn=aws_iam_role.eks_cluster_role.arn
 
 vpc_config{
 subnet_ids=[
-aws_subnet.subnet1.id,
-aws_subnet.subnet2.id
+aws_subnet.private.id,
+aws_subnet.public.id
 ]
 }
 
